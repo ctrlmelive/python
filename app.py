1f70573081cf59d44904812cfdb7d3e1b24a4e4d
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2023-05-12 00:03:16 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Name: rongoJustin\nOccupation: Programmer\nHobbies: writing code\nFavorite website: www.google.com\nContact:rongoaa@Gmail.com'
+    return 'Name: rongoJustion\nOccupation: Programmer\nHobbies: writing code\nFavorite website: www.google.com\nContact:rongoaa@Gmail.com'
 
 
 if __name__ == "__main__":
